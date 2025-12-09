0b2b837e6312187210b277f8ad15f1865d2cef14
mariapaulavera <mariapaulaveram@gmail.com>
2025-12-09 11:00:15 -0300

    cambio

--- reclamos-municipales-react/vite.config.js
+++ reclamos-municipales-react/vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reclamos-municipales-react/',
+  //base: '/reclamos-municipales-react/',
+  base: '/',   // 👈 importante: raíz
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
     }
   }
 });
+
+
